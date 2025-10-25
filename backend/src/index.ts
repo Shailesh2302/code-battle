3bb4630a660314eecd2216699d1b429c6dfd1ffb
rushikesh <153095920+Shailesh2302@users.noreply.github.com>
2025-10-25 10:39:53 +0530

    add some changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,18 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
-const PORT = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from Express + TypeScript + pnpm!");
-});
- 
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(8080, () => {
+  console.log("listening on port 8080");
 });
